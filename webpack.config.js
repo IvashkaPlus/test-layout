d92ecfa83af929ddc18df78355b689fa4ddaee63
Ivan Plyusnin <plyusnin.ivan.v@gmail.com>
2021-03-09 13:58:08 +0300

    SVG loader, header layout

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,17 @@ module.exports = {
                     loader: 'sass-loader'
                 }]
             },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
         ]
     }
 };
